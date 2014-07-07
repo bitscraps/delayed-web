5a3af1c7b564310d2531720a4dc631d2d4995e52
Graham Hadgraft <graham.hadgraft@stylefoundry.co.uk>
2014-07-07 13:11:27 +0100

    nokogiri processing trains page

--- app/controllers/api/trains_controller.rb
+++ app/controllers/api/trains_controller.rb
@@ -1,5 +1,19 @@
 class Api::TrainsController < ApplicationController
-	def index
-		render json: {}
+  require 'nokogiri'
+  require 'open-uri'
+
+  def index
+    trains = []
+    web_page = Nokogiri::HTML(open("http://ojp.nationalrail.co.uk/service/timesandfares/#{params[:from]}/#{params[:to]}/today/#{params[:time]}/dep"))
+
+    web_page.css('#oft tbody .mtx').each do |link|
+      from = link.css('.from').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\]/, "").gsub(/\<span.*\/span\//, "")
+      to = link.css('.to').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\]/, "").gsub(/\<span.*\/span\//, "")
+      dep = link.css('.dep').text.gsub(/\n/, "").gsub(/\t/, "")
+      arr = link.css('.dep').text.gsub(/\n/, "").gsub(/\t/, "")
+      trains << {from: from , to: to, dep: dep, arr: arr}
+    end
+    puts trains.inspect
+    render json: {}
 	end
 end
--- spec/controllers/api/trains_controller_spec.rb
+++ spec/controllers/api/trains_controller_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 
 describe Api::TrainsController do
   describe "GET" do
-    it "returns ok" do
-      get 'index'
-      expect(response).to be_ok
+    it "returns a json array of trains" do
+      get 'index', {:from => "IPS", :to => "SMK", :time => "0830"}
+      times = JSON.parse(response.body)
+      expect(times.first["dep"]).to eq("08:44")
+      expect(times.first["from"]).to eq("Ipswich")
+      expect(times.first["to"]).to eq("Stowmarket")
+      expect(times.first["arr"]).to eq("08:55")
+
     end
   end
 
