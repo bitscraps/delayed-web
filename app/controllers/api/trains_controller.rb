b3f7e85ce6498f638a4ed9cde0335d9b796842ce
Graham Hadgraft <graham.hadgraft@gmail.com>
2014-07-20 16:16:40 +0100

    trains specs

--- app/controllers/api/trains_controller.rb
+++ app/controllers/api/trains_controller.rb
@@ -3,20 +3,24 @@ class Api::TrainsController < ApplicationController
   require 'open-uri'
 
   def index
-    trains = []
-    web_page = Nokogiri::HTML(open("http://ojp.nationalrail.co.uk/service/timesandfares/#{params[:from]}/#{params[:to]}/today/#{params[:time]}/dep"))
-
-    web_page.css('#oft tbody .mtx').each do |link|
-      from = link.css('.from').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\].*/, "").lstrip.chop
-      to = link.css('.to').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\].*/, "").lstrip.chop
-      from_code = link.css('.from').text.match(/\[(.*?)\]/)
-      from_code = $1
-      to_code = link.css('.to').text.match(/\[(.*?)\]/)
-      to_code = $1
-      dep = link.css('.dep').text.gsub(/\n/, "").gsub(/\t/, "")
-      arr = link.css('.arr').text.gsub(/\n/, "").gsub(/\t/, "")
-      trains << {from: from , to: to, dep: dep, arr: arr, from_code: from_code, to_code: to_code}
-    end
-    render json: trains
-	end
+    if params["from"] && params["to"] && params["time"]
+      trains = []
+      puts "http://ojp.nationalrail.co.uk/service/timesandfares/#{params["from"]}/#{params["to"]}/today/#{params["time"]}/dep"
+      web_page = Nokogiri::HTML(open("http://ojp.nationalrail.co.uk/service/timesandfares/#{params["from"]}/#{params["to"]}/today/#{params["time"]}/dep"))
+      web_page.css('#oft tbody .mtx').each do |link|
+        from = link.css('.from').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\].*/, "").lstrip.chop
+        to = link.css('.to').text.gsub(/\n/, "").gsub(/\t/, "").gsub(/\[.*\].*/, "").lstrip.chop
+        from_code = link.css('.from').text.match(/\[(.*?)\]/)
+        from_code = $1
+        to_code = link.css('.to').text.match(/\[(.*?)\]/)
+        to_code = $1
+        dep = link.css('.dep').text.gsub(/\n/, "").gsub(/\t/, "")
+        arr = link.css('.arr').text.gsub(/\n/, "").gsub(/\t/, "")
+        trains << {from: from , to: to, dep: dep, arr: arr, from_code: from_code, to_code: to_code}
+      end
+      render json: trains
+    else 
+      render json: {error: "missing some or all parameters", text: "Please ensure that you have passed the 'from', 'to' and 'time' parameters."}, status: 400
+	   end
+  end
 end
--- spec/controllers/api/trains_controller_spec.rb
+++ spec/controllers/api/trains_controller_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe Api::TrainsController do
   describe "GET" do
     it "returns a json array of trains" do
-      get 'index', {:from => "IPS", :to => "SMK", :time => "0830"}
+      get 'index', {"from" => "IPS", "to" => "SMK", "time" => "0830"}
       times = JSON.parse(response.body)
       expect(times.first["dep"]).to eq("08:44")
       expect(times.first["to_code"]).to eq("SMK")
@@ -12,6 +12,12 @@ describe Api::TrainsController do
       expect(times.first["from_code"]).to eq("IPS")
       expect(times.first["to"]).to eq("Stowmarket ")
       expect(times.first["arr"]).to eq("08:55")
+    end
+
+    it "returns error if not passed all params" do
+      get 'index'
+      expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)["error"]).to eq("missing some or all parameters")
     end
   end
 
