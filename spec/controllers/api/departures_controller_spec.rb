5fd01ed09a0fa0aa8b00e0ab5867d94a68634774
Graham Hadgraft <graham.hadgraft@stylefoundry.co.uk>
2014-08-05 13:20:25 +0100

    updated test

--- spec/controllers/api/departures_controller_spec.rb
+++ spec/controllers/api/departures_controller_spec.rb
@@ -6,7 +6,7 @@ describe Api::DeparturesController do
     it "returns JSON containing the departure requested" do
       get 'index', {"from" => "SMK", "to" => "IPS", "time" => "13:14"}
       times = JSON.parse(response.body)
-      expect(times["status"]).to eq("13:16")
+      expect(times["status"]).to eq("--:--")
     end
   end
 
